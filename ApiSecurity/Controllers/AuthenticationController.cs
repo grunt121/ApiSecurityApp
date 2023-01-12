802f2e776503829873d117623610277260ef43dc
Jamie Garner <jamie@ncass.org.uk>
2023-01-12 15:02:47 +0000

    Add project files.

---
+++ ApiSecurity/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSecurity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public record AuthenticateData(string? UserName, string? Password);
+
+        //[HttpPost("token")]
+        //public ActionResult<string> Authenticate([FromBody] string data)
+        //{
+
+        //}
+    }
+}
