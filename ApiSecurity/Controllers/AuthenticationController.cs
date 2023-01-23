53d8d7fa74f6701594da8de61e113c215afed4a5
Jamie Garner <jamie@ncass.org.uk>
2023-01-23 17:05:48 +0000

    Trying out simple Authentication example

--- ApiSecurity/Controllers/AuthenticationController.cs
+++ ApiSecurity/Controllers/AuthenticationController.cs
@@ -8,10 +8,37 @@ namespace ApiSecurity.Controllers
     {
         public record AuthenticateData(string? UserName, string? Password);
 
-        //[HttpPost("token")]
-        //public ActionResult<string> Authenticate([FromBody] string data)
-        //{
+        public record UserData(int UserId, string Username);
 
-        //}
+        [HttpPost("token")]
+        public ActionResult<string> Authenticate([FromBody] AuthenticateData data)
+        {
+
+
+        }
+
+        private static UserData? ValidateCredentials(AuthenticateData data)
+        {
+            //Simple Demo
+            if (CompareValues(data.UserName, "jamie") && CompareValues(data.Password,"1234"))
+                return new UserData(1,data.UserName!);
+
+            if (CompareValues(data.UserName, "peterparker") && CompareValues(data.Password, "1234"))
+                return new UserData(2, data.UserName!);
+
+            return null;
+
+        }
+
+        private static bool CompareValues(string? actual, string expected)
+        {
+            if (actual is null)
+                return false;
+
+            if (actual.Equals(expected,StringComparison.InvariantCultureIgnoreCase))
+                return true;
+
+            return false;
+        }
     }
 }
