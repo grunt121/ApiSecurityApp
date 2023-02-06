8299d0444787d7ea9b183fe917c7e719808cde99
Jamie Garner <jamie@ncass.org.uk>
2023-02-06 17:51:11 +0000

    Example of creating a Bearer token

--- ApiSecurity/Controllers/AuthenticationController.cs
+++ ApiSecurity/Controllers/AuthenticationController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace ApiSecurity.Controllers
 {
@@ -6,6 +11,12 @@ namespace ApiSecurity.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public record AuthenticateData(string? UserName, string? Password);
 
         public record UserData(int UserId, string Username);
@@ -19,11 +30,36 @@ namespace ApiSecurity.Controllers
             if (user == null)
                 return Unauthorized();
 
+            var token = GenerateToken(user);
+            return Ok(token);
 
 
         }
 
-        private GenerateUserToken()
+        private string GenerateToken(UserData user)
+        {
+            var secretKey =
+                new SymmetricSecurityKey(
+                    Encoding.ASCII.GetBytes(_configuration.GetValue<string>("Authentication:SecretKey")));
+            var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claims = new()
+            {
+                new(JwtRegisteredClaimNames.Sub, user.UserId.ToString()),
+                new(JwtRegisteredClaimNames.UniqueName, user.Username)
+            };
+
+            var token = new JwtSecurityToken(
+                _configuration.GetValue<string>("Authentication:Issuer"),
+                _configuration.GetValue<string>("Authentication:Audience"),
+                claims,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddMinutes(1),
+                signingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
 
         private static UserData? ValidateCredentials(AuthenticateData data)
         {
